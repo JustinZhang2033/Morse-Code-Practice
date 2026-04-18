ea962f856bda6afdb1627cf8c96986bd015216f2
Justin Zhang <justinzhang2033@gmail.com>
2026-04-17 22:47:04 -0700

    Allowed user testing on encode

--- MorseDictionary.java
+++ MorseDictionary.java
@@ -1,10 +1,14 @@
 //Language transfer to Morse Code and English
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
 
 public class MorseDictionary {
     private static final HashMap<String, String> decoder = new HashMap<>();
     private static final HashMap<String, String> encoder = new HashMap<>();
+    static final ArrayList<String> letters = new ArrayList<>();
+    static final ArrayList<String> mcodes = new ArrayList<>();
+
     static {
         decoder.put(".-", "A");
         decoder.put("-...", "B");
@@ -37,6 +41,63 @@ public class MorseDictionary {
         for (Map.Entry<String, String> entry: decoder.entrySet()) {
             encoder.put(entry.getValue(), entry.getKey());
         }
+
+        
+        letters.add("A");
+        letters.add("B");
+        letters.add("C");
+        letters.add("D");
+        letters.add("E");
+        letters.add("F");
+        letters.add("G");
+        letters.add("H");
+        letters.add("I");
+        letters.add("J");
+        letters.add("K");
+        letters.add("L");
+        letters.add("M");
+        letters.add("N");
+        letters.add("O");
+        letters.add("P");
+        letters.add("Q");
+        letters.add("R");
+        letters.add("S");
+        letters.add("T");
+        letters.add("U");
+        letters.add("V");
+        letters.add("W");
+        letters.add("X");
+        letters.add("Y");
+        letters.add("Z");
+        letters.add(" ");
+
+        mcodes.add(".-");
+        mcodes.add("-...");
+        mcodes.add("-.-.");
+        mcodes.add("-..");
+        mcodes.add(".");
+        mcodes.add("..-.");
+        mcodes.add("--.");
+        mcodes.add("....");
+        mcodes.add("..");
+        mcodes.add(".---");
+        mcodes.add("-.-");
+        mcodes.add(".-..");
+        mcodes.add("--");
+        mcodes.add("-.");
+        mcodes.add("---");
+        mcodes.add(".--.");
+        mcodes.add("--.-");
+        mcodes.add(".-.");
+        mcodes.add("...");
+        mcodes.add("--");
+        mcodes.add("..-");
+        mcodes.add("...-");
+        mcodes.add(".--");
+        mcodes.add("-..-");
+        mcodes.add("-.--");
+        mcodes.add("--..");
+        mcodes.add("/");
     }
 
     public static String decode(String code) {
@@ -45,5 +106,20 @@ public class MorseDictionary {
 
     public static String encode(String text) {
         return encoder.get(text);
+    }
+
+    public static String encodeMessage(String m) {
+        String[] mArr = m.split("");
+        String encoder = "";
+        String result = "";
+        for (String s : mArr) {
+            if (!(s.equals(" "))) {
+                encoder = MorseDictionary.encode(s) + " ";
+            } else {
+                encoder = "/ ";
+            }
+            result += encoder;
+        }
+        return result;
     }
 }
--- main.java
+++ main.java
@@ -4,31 +4,45 @@
 import java.util.Scanner;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class main {
     public static void main(String[] args) {
         Scanner scanner  = new Scanner(System.in);
-
+        Random rand = new Random();
+        
+        int randomNumber;
         String input = "";
         String translatedMessage = "";
         String addedEncoder = "";
+        String randomMessage = "";
+        String correctAnswer = "";
+        
 
         System.out.print("Would you like to encode or decode? (E/D)");
         String type = scanner.nextLine();
 
         if (type.equals("E") || type.equals("e")) {
-            System.out.println("What would you like to encode?");
-            input = scanner.nextLine().toUpperCase();
-            String[] message = input.split("");
+            System.out.println("You have chosen to encode");
+            System.out.println("How many characters would you like to encode?");
+            int numCharEncode = Integer.parseInt(scanner.nextLine());
 
-            for (String s : message) {
-                if (!(s.equals(" "))) {
-                    addedEncoder = MorseDictionary.encode(s) + " ";
-                } else {
-                    addedEncoder = MorseDictionary.encode(s);
-                }
-                translatedMessage += addedEncoder;
+            for (int i = 0; i < numCharEncode; i++) {
+                randomMessage += MorseDictionary.letters.get(rand.nextInt(27));
             }
+
+            correctAnswer = (MorseDictionary.encodeMessage(randomMessage));
+            System.out.println("Here is your random message \n" + "|" + randomMessage + "|");
+            System.out.println("Please enter your translation");
+            input = scanner.nextLine();
+
+            if (input.equals(correctAnswer) || (input.equals(correctAnswer.substring(0, correctAnswer.length() - 1)))) {
+                System.out.println("YAY");
+            } else {
+                System.out.println("RIP");
+            }
+
+
         } else if (type.equals("D") || type.equals("d")) {
             System.out.println("What would you like to decode?");
             input = scanner.nextLine();
