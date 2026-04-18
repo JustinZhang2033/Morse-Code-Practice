ba1399065724a539c046698f412548855a647b99
JustinZhang2033 <justinzhang2033@gmail.com>
2026-04-17 20:55:44 -0700

    Initial Commit

---
+++ MorseDictionary.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MorseDictionary {
+    private static final HashMap<String, String> decoder = new HashMap<>();
+    private static final HashMap<String, String> encoder = new HashMap<>();
+    static {
+        decoder.put(".-", "A");
+        decoder.put("-...", "B");
+        decoder.put("-.-.", "C");
+        decoder.put("-..", "D");
+        decoder.put(".", "E");
+        decoder.put("..-.", "F");
+        decoder.put("--.", "G");
+        decoder.put("....", "H");
+        decoder.put("..", "I");
+        decoder.put(".---", "J");
+        decoder.put("-.-", "K");
+        decoder.put(".-..", "L");
+        decoder.put("--", "M");
+        decoder.put("-.", "N");
+        decoder.put("---", "O");
+        decoder.put(".--.", "P");
+        decoder.put("--.-", "Q");
+        decoder.put(".-.", "R");
+        decoder.put("...", "S");
+        decoder.put("-", "T");
+        decoder.put("..-", "U");
+        decoder.put("...-", "V");
+        decoder.put(".--", "W");
+        decoder.put("-..-", "X");
+        decoder.put("-.--", "Y");
+        decoder.put("--..", "Z");
+        decoder.put("/", " ");
+
+        for (Map.Entry<String, String> entry: decoder.entrySet()) {
+            encoder.put(entry.getValue(), entry.getKey());
+        }
+    }
+
+    public static String decode(String code) {
+        return decoder.get(code);
+    }
+
+    public static String encode(String text) {
+        return encoder.get(text);
+    }
+}
---
+++ main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner  = new Scanner(System.in);
+
+        String input = "";
+        String translatedMessage = "";
+        String addedEncoder = "";
+
+        System.out.print("Would you like to encode or decode? (E/D)");
+        String type = scanner.nextLine();
+
+        if (type.equals("E") || type.equals("e")) {
+            System.out.println("What would you like to encode?");
+            input = scanner.nextLine().toUpperCase();
+            String[] message = input.split("");
+
+            for (String s : message) {
+                if (!(s.equals(" "))) {
+                    addedEncoder = MorseDictionary.encode(s) + " ";
+                } else {
+                    addedEncoder = MorseDictionary.encode(s);
+                }
+                translatedMessage += addedEncoder;
+            }
+        } else if (type.equals("D") || type.equals("d")) {
+            System.out.println("What would you like to decode?");
+            input = scanner.nextLine();
+            String[] message = input.split(" ");
+
+            for (String s : message) {
+                translatedMessage += MorseDictionary.decode(s.toUpperCase());
+            }
+        }
+
+        System.out.println(translatedMessage);
+    }
+}
